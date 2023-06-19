7b6e8a067adef20937f22090a6eba9c6cdffb3e1
Chris Johnson <chris.johnson@senecacollege.ca>
2023-06-19 10:06:42 -0400

    Initial course code replacement.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,19 +5,19 @@ const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OER Course Title',
-  tagline: 'OER Tag/Keyword',
+  title: 'OSL740 - Administration of Open Source Systems',
+  tagline: 'OSL740',
   url: 'https://Seneca-ICTOER.github.io/',
-  baseUrl: '/OERTemplate/',
+  baseUrl: '/OSL740/',
   trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'Seneca-ICTOER',
-  projectName: 'OERTemplate',
+  projectName: 'OSL740',
   themeConfig: {
     navbar: {
-      title: 'OER Course Title',
+      title: 'OSL740 - Administration of Open Source Systems',
       logo: {
         alt: 'Seneca College',
         src: 'img/logo.svg',
@@ -31,7 +31,7 @@ module.exports = {
       style: 'dark',
       links: [
         {
-          title: 'OER Course Title',
+          title: 'OSL740 - Administration of Open Source Systems',
           items: [
             {
               label: 'Contents',
@@ -57,7 +57,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: 'https://github.com/Seneca-ICTOER/OERTemplate/tree/main',
+          editUrl: 'https://github.com/Seneca-ICTOER/OSL740/tree/main',
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
